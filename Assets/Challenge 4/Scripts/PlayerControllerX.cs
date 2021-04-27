b986b3f0e05202b158f7f5654e52348bad3ee576
Bekay1211 <82642680+IsThisTheCrustyCrab-was-taken@users.noreply.github.com>
2021-04-27 16:09:44 +0200

    Assignment completed :)

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -6,6 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
     private float speed = 500;
+    public float boostMultiplier = 3;
     private GameObject focalPoint;
 
     public bool hasPowerup;
@@ -14,9 +15,9 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
-
-    public ParticleSystem boostParticles;
     
+    public ParticleSystem boostParticles;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -27,10 +28,13 @@ public class PlayerControllerX : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
-
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        // Boost player when he presses the button
+        if(Input.GetButtonDown("Jump")){boost();}
+        // Let smoke track player
+        boostParticles.gameObject.transform.position = transform.position;
 
     }
 
@@ -42,6 +46,8 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            CancelInvoke("PowerupCooldown");
+            Invoke("PowerupCooldown", powerUpDuration);
         }
     }
 
@@ -57,7 +63,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
@@ -72,6 +78,10 @@ public class PlayerControllerX : MonoBehaviour
         }
     }
 
-
-
+    // boosts the player
+    void boost()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * boostMultiplier, ForceMode.Impulse);
+        boostParticles.Play();
+    }
 }
